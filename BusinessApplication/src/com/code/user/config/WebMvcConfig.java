5d1608b131cf59c2ef3b9b9c8b39242eea3a37e8
VanajaKillada <29101996k@gmail.com>
2019-07-03 19:48:46 +0530

    Configured Spring mvc.

--- BusinessApplication/src/com/code/user/config/WebMvcConfig.java
+++ BusinessApplication/src/com/code/user/config/WebMvcConfig.java
@@ -1,5 +1,23 @@
 package com.code.user.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.code.java")
 public class WebMvcConfig {
 
+	@Bean(name="viewRsolver")
+	public InternalResourceViewResolver getViewResolver()
+	{
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+		
+	}
+	
 }
